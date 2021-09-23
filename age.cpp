d30a7485746ba8631021faee43bcf7a08511c1dc
meenuteneti <87061873+meenuteneti@users.noreply.github.com>
2021-09-23 18:49:31 +0530

    Add files via upload

---
+++ age.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int age;
+	cout<<"enter your age:";
+	cin>>age;
+	if(age<=10)
+	{
+		cout<<"you are not eligible for swimming";
+	}
+	cout<<"you are eligible for swimming";
+	return 0;
+	
+	
+}
